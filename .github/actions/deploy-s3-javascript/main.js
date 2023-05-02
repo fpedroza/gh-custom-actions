ba82db43968b682a7d875cfefebf7ab0459fab0d
Frank Pedroza <f.pedroza@elsevier.com>
2023-05-02 11:21:23 -0600

    use aws cli to upload

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,18 @@ const exec = require("@actions/exec");
 
 function run() {
     core.notice("running main.js from custom action");
+
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // github.getOctokit()
+    // github.context.
+
+    const s3Uri = `s3://${bucket}`;
+
+    exec.exec(`aws s3 sync ${s3Uri} ${distFolder} --region ${bucketRegion}`);
+
 }
 
 run();
